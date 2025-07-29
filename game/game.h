ef80b3d0d598f0ca46655a62563f2eed952e4eb0
Jens Joschinski <j.joschinski@gmail.com>
2025-07-29 09:17:50 +0200

    separate API from game.h
    PIMPL

--- game/game.h
+++ game/game.h
@@ -2,22 +2,9 @@
 
 #include "options.h"
 #include "inputSettings.h"
-#include "ball.h"
-#include "paddle.h"
-#include "controller.h"
-#include "enums.h"
-#include "gameState.h"
-#include "observer.h"
-#include "renderer.h"
-#include "sound.h"
 
 /** @cond */
-#include <memory>
 #include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/System.hpp>
-#include <variant>
-#include <vector>
 /** @endcond */
 
 #ifdef BUILDING_GAME_DLL
@@ -26,6 +13,7 @@
 #define GAME_API __declspec(dllimport)
 #endif
 
+class GameImpl;
 /*! 
     *  \brief Game class
     *  \details
@@ -43,43 +31,11 @@ class Game{
 	 */
 	GAME_API explicit Game(const Options& options, InputSettings p1, InputSettings p2, sf::RenderWindow& window);
 
-    GAME_API ~Game() = default;
+    GAME_API ~Game();
 
     GAME_API void run();
 
-    /**
-	* @brief run 1 time step
-	 * returns true if game over.
-	 */
-    bool update(const std::vector<sf::Event>& events, const sf::Time& elapsed);
-
-    private:
-        // pimpl - to do
-    sf::RenderWindow* m_window;
-	GameState m_gameState; /**< contains current state (position etc) of all game objects */
-	Renderer m_renderer;
-	std::unique_ptr<Controller> m_c1; /**< Controller for player 1. May be controlled by mouse, keyboard, AI .. */
-	std::unique_ptr<Controller> m_c2; /**< Controller for player 2. May be controlled by mouse, keyboard, AI .. */
-    Paddle m_pLeft;
-    Paddle m_pRight;
-    std::unique_ptr<Ball> m_ball;
-
-    int m_score1;
-    sf::Text m_score1Text;
-    int m_score2;
-    sf::Text m_score2Text;
-    double m_ballVelocity;
-    sf::Font m_font;
-
-    Sound m_sound;
-    void addBall(double speed);
-    /**
-	* @brief Check for collisions and update game
-    * Updates points, rebounces ball if neccessary
-	* returns true if someone scored
-	*/
-    bool handleCollisions(const sf::Time& elapsed);
-    void movePlayer(Paddle& paddle, Controller* control, const std::vector<sf::Event>& events, const sf::Time& elapsed);
-    std::unique_ptr<Controller> createController(InputSettings setup, sf::RenderWindow& window);
+private:
+	std::unique_ptr<GameImpl> m_impl;
 };
 
